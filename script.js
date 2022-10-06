72e69d58d793c0c0d4ebfdc1d04f1905c70aaf17
Valentin Di Geronimo <digeronimovalentin@gmail.com>
2022-10-06 01:40:46 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,12 +5,10 @@ const TheBeatles = ['3WrFJ7ztbogyGnTHbHJFl2', 10]
 const SodaStereo = ['7An4yvF7hDYDolN4m5zKBp', 7]
 const LosRedondos = ['6byQKddO1b34lXC2ZEjehQ', 0]
 
-const ALBUMS = await Api.REQ_ALBUMS(...TheBeatles) //todos los albums
-console.log(ALBUMS)
+const ALBUMS = await Api.REQ_ALBUMS(...TheBeatles)
 
 document.querySelector('.button_play').addEventListener('click', async() => {
     const conts = document.querySelectorAll('.cont')
-
     const arr = Aux.nsRandoms(ALBUMS)
     const x = arr[Aux.random(conts)]
 
@@ -21,4 +19,3 @@ document.querySelector('.button_play').addEventListener('click', async() => {
 
     Aux.repSpotify(TRACKS[y].id)
 })
-
