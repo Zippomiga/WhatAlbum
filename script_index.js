5c9ca26ed17a21ef88e06342dc1b09839da028dd
Valentin Di Geronimo <digeronimovalentin@gmail.com>
2022-10-20 04:40:40 -0300

    Add files via upload

--- script_index.js
+++ script_index.js
@@ -9,7 +9,11 @@ input_ID.addEventListener('click', () => {
 
 btnCapturar_ID.addEventListener('click', async(event) => {
     event.preventDefault()
-    const ID = document.querySelector('.input-ID').value
+
+    const valor = document.querySelector('.input-ID').value
+    const digitos_ID = 22
+    const ID = valor.slice(valor.length - digitos_ID , valor.length)
+
     const ALBUMS = await Api.REQ_ALBUMS(ID)
 
     if(input_ID.value === '') {
